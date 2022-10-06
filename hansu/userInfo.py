d2a2872a8211ffb9b3f7ed6681e187787e81e250
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2022-10-06 16:36:47 +0900

    modul

--- hansu/userInfo.py
+++ hansu/userInfo.py
@@ -25,10 +25,6 @@ class UserInfo :
         print('사용자 현제 위치 :', self.address)
         print(f'위도 : {self.loc["lat"]}, 경도 : {self.loc["lng"]}')
 
-
-
-
-
 # user1 = UserInfo.geocoding('대구광역시 산격동 글로벌플라자')
 # user1.get_user_info()
         
